28c9d1e549b413939533707e98b77c693c17941a
cihanerman <cihan.erman@outlook.com>
2018-02-22 02:13:11 +0300

    VueJs eğitimi 1. gün

---
+++ vuelearn/src/store/index.js
@@ -0,0 +1,25 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+new Vuex.Store({
+    state: {
+        pruducts: []
+    },
+    getters: {
+        productsCount() {
+
+        }
+    },
+    actions: {
+        fetchProduct() {
+
+        }
+    },
+    mutations: {
+        setProducys() {
+
+        }
+    }
+})
